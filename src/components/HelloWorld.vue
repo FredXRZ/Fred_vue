565867f8962e730c11ec15f4dd48a07f823d4aed
FredXRZ <395432262@qq.com>
2018-11-02 15:41:35 +0800

    项目第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div class="home_content">
+      <img class="home_img" src="../assets/logo.png" alt="">
+      <a class="run_in_btn">进入</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    width: 100%;
+    height: 100%;
+    background: #358;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .home_img{
+    width: 300px;
+    height: 300px;
+  }
+  .home_content{
+    text-align: center;
+  }
+  .run_in_btn{
+    color: #fff;
+    border: 1px solid #fff;
+    display: block;
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p @click="changeMessage" class="my_msg">{{msg}}</p>
+    <ul v-if="showList">
+      <li v-for="item in arr" :key="item.id">{{item}}</li>
+    </ul>
+    <button @click="changeArr">修改数组</button>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        msg:`This is Fred's message!`,
+        arr:[0,1,2,3,4,5,6],
+        showList:true
+      }
+    },
+    methods:{
+      changeMessage:function(){
+        this.msg = `I have changed my message!`
+      },
+      changeArr:function(){
+        this.arr.reverse();
+      }
+    }
+  }
+</script>
+<style>
+  .my_msg{
+    font-size: 30px;
+    color: #626;
+    background: #358;
+    border: 1px solid #f00;
+    border-radius: 10px;
+  }
+</style>
+
+
