3aec7b7c8d01224d2fd9c43ceef2698d79994adf
FredXRZ <395432262@qq.com>
2018-12-21 18:03:30 +0800

    调教

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,27 @@
 <template>
     <div class="recommend" >
+        <swiper :options="swiperOption"  ref="mySwiper">
+			<!-- 这部分放你要渲染的那些内容 -->
+			<swiper-slide v-for="(item,index) in items" :key="index">
 
+			</swiper-slide>
+			<!-- 这是轮播的小圆点 -->
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
     </div>
 </template>
 <script>
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
     export default {
-    
+        data() {
+            return {
+                msg:'welcom'
+            }
+        },
+        components: {
+			swiper,
+			swiperSlide
+		}
     }
 </script>
 <style lang="scss" scoped>
